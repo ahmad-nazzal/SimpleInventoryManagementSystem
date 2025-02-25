c92ddc0ddfeb755cb2e9df854bb60bcbc7c694ac
Ahmad <nazzala8@gmail.com>
2025-02-25 17:45:50 +0200

    Create Product Class with properties and a constructor

---
+++ ProductManagement/Product.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simple_Inventory_Management_System.ProductManagement
+{
+    public class Product
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public int Quantity { get; set; }
+
+        public Product(string name, double price, int quantity)
+        {
+            Name = "";
+            Price = 0;
+            Quantity = 0;
+        }
+
+    }
+}
