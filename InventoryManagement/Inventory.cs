5c1db8fcc58a8629e9bc3038c01bcef63ba3c988
Ahmad <nazzala8@gmail.com>
2025-02-25 18:55:08 +0200

    Create Inventory Class with Products Property and a Constructor

---
+++ InventoryManagement/Inventory.cs
@@ -0,0 +1,21 @@
+﻿using Simple_Inventory_Management_System.ProductManagement;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Simple_Inventory_Management_System.InventoryManagement
+{
+    public class Inventory
+    {
+        public List<Product> Proudcts { get; }
+
+        public Inventory()
+        {
+            Proudcts = new List<Product>();
+        }
+
+
+    }
+}
