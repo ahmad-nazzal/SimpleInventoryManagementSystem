cba11978486b7613dcf080361d5b38f68bfe098c
Ahmad <nazzala8@gmail.com>
2025-02-26 20:40:43 +0200

    Added validation to Price and Quantity properties in Product Class to prevent negative values

--- ProductManagement/Product.cs
+++ ProductManagement/Product.cs
@@ -8,9 +8,33 @@ namespace Simple_Inventory_Management_System.ProductManagement
 {
     public class Product
     {
+        private double _price;
+        private int _quantity;
         public string Name { get; set; }
-        public double Price { get; set; }
-        public int Quantity { get; set; }
+        public double Price
+        {
+            get => _price;
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException("Price cannot be negative");
+                }
+                _price = value;
+            }
+        }
+        public int Quantity
+        {
+            get => _quantity;
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException("Quantity cannot be negative");
+                }
+                _quantity = value;
+            }
+        }
 
         public Product(string name, double price, int quantity)
         {
